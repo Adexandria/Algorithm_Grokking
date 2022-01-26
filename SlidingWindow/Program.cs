f22e87bdcc8bfcc711e5453c8e56c0f7eaf1417e
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-01-26 09:39:48 +0100

    Distinct Characters

---
+++ SlidingWindow/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DistinctCharacters("cbbebi", 3));
+          
+
+        }
+        public static int DistinctCharacters(string s, int k)
+        {
+            string windowSum = string.Empty;
+            int maxLength = int.MinValue;
+            int windowStart = 0;
+            for (int windowEnd = 0; windowEnd < s.Length; windowEnd++)
+            { 
+                windowSum += s[windowEnd];
+                var count = windowSum.Distinct().Count();
+                if (count > k)
+                {
+                    windowStart++;
+                    windowSum = s[windowStart].ToString();
+                }
+                if (windowSum.Length > maxLength)
+                {
+                        maxLength = windowSum.Length;
+                }      
+            }
+            return maxLength;
+
+        }
+        
+    }
+}
